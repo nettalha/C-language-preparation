4fe48595251aa168039c78b11086414d49e5703f
Noureddine Ettalhaouy <nettalha@c2r6p6.1337.ma>
2022-04-13 20:53:52 +0000

    first_word

---
+++ Level-00/first_word.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+int main (int ac, char **av)
+{
+	if (ac == 2)
+	{
+		if (ac != 2)
+		{
+			write(1, "\n", 1);
+			return (1);
+		}
+		while (*av[1] && (*av[1] == ' ' || *av[1] == '	'))
+			av[1]++;
+		while (*av[1] && (*av[1] != ' ' && *av[1] != '	'))
+		{
+			write(1, av[1], 1);
+			av[1]++;
+		}
+		write(1, "\n", 1);
+		return (0);
+	}
+}
