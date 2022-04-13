f802150a8217344ff56b9785c75b16a5a86319fb
Noureddine Ettalhaouy <nettalha@c2r7p1.1337.ma>
2022-04-13 13:46:39 +0000

    ft_strlen

---
+++ Level-00/ft_strlen.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int i;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+int main ()
+{
+	char str [] = "HelloWorld";
+	printf("%d", ft_strlen(str));
+}
