07e1918e2a5e88a3c44ccc0048526b53a36d109d
Noureddine Ettalhaouy <nettalha@c2r6p7.1337.ma>
2022-04-14 18:05:43 +0000

    ft_strdup

---
+++ Level-01/ft_strdup.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+int ft_strlen(char *str)
+	{
+		int i = 0;
+		while (str[i])
+		{
+			i++;
+		}
+		return (i);
+	}
+
+char	*ft_strdup(char *src)
+{
+	int i = 0;
+	char  *arr;
+	arr = (char*) malloc(ft_strlen(src) * sizeof(char) + 1);
+	if (!arr)
+	{
+		return (NULL);
+	}
+	while (src[i])
+	{
+		arr[i] = src[i];
+		i++;
+	}
+	arr[i] = '\0';
+	return (arr);
+}
